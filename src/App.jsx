275e231b114884f70b85fa7a663c849d65dd7418
AhmedAttia0 <AhmedAboulenein0147@gmail.com>
2026-03-17 19:56:02 +0200

    admin-user

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './page/Home'
-import Contact from './page/Contactus';
-import LoginForm from './page/Login';
-import RegisterForm from './page/Register';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./page/Home";
+import Contact from "./page/Contactus";
+import LoginForm from "./page/Login";
+import RegisterForm from "./page/Register";
+import Dashboard from "./admin/pages/dashboard/Dashboard";
+import Artists from "./admin/pages/artists/Artists";
+import Artworks from "./admin/pages/artworks/Artworks";
+import Users from "./admin/pages/users/Users";
+import Auctions from "./admin/pages/auctions/Auctions";
+import AdminMain from "./admin/pages/adminMain/AdminMain";
 function App() {
-
   return (
     <>
-      
-      <div className=""> {/* To avoid content hidden behind fixed navbar */}
+      <div className="">
+        {" "}
+        {/* To avoid content hidden behind fixed navbar */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<LoginForm />} />
-            <Route path="/register" element={<RegisterForm />} />
-    
+          <Route path="/register" element={<RegisterForm />} />
+          {/* Admin Routes */}
+          <Route path="/admin/" element={<Dashboard />}>
+            <Route index element={<AdminMain />} />
+            <Route path="artists" element={<Artists />} />
+            <Route path="artworks" element={<Artworks />} />
+            <Route path="users" element={<Users />} />
+            <Route path="auctions" element={<Auctions />} />
+          </Route>
         </Routes>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
